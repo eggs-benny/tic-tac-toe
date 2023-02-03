5dea629379db2b4b7e7df189b96b6af5338281fe
eggs-benny <benedict.smith87@gmail.com>
2023-02-03 15:43:29 +0000

    added automatic player change

--- src/game/game.js
+++ src/game/game.js
@@ -1,15 +1,28 @@
 class Game {
   constructor() {
     this.grid = [[null, null, null],[null, null, null],[null, null, null]]
+    this.symbol = 'X'
   }
 
-  addSymbol(coords, symbol) {
+  addSymbol(coords) {
+    // let target = 
     if (this.grid[coords[0]][coords[1]] != null) {throw new Error("Error, can't take turn in same position"); 
   } else {
-    this.grid[coords[0]][coords[1]] = symbol
+    this.grid[coords[0]][coords[1]] = this.symbol
     return this.grid}
   }
 
+  changeSymbol(currentSymbol) {
+    if (currentSymbol === 'O') {
+      this.symbol = 'X'
+    } else {
+      this.symbol = 'O'
+    }
+  } 
 
+  takeTurn(coords) {
+    this.changeSymbol(this.symbol)
+    this.addSymbol(coords)
+  }
 } 
 module.exports = Game;
