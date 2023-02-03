c6fd0c4afbbaf917719fc1dfde9e85c8cdd6092a
eggs-benny <benedict.smith87@gmail.com>
2023-02-03 14:52:38 +0000

    throw error when in same place

--- src/game/game.js
+++ src/game/game.js
@@ -4,9 +4,12 @@ class Game {
   }
 
   addSymbol(coords, symbol) {
+    if (this.grid[coords[0]][coords[1]] != null) {throw new Error("Error, can't take turn in same position"); 
+  } else {
     this.grid[coords[0]][coords[1]] = symbol
-    return this.grid
+    return this.grid}
   }
+
 
 } 
 module.exports = Game;
--- src/game/game.test.js
+++ src/game/game.test.js
@@ -9,11 +9,29 @@ describe('Game', () => {
       expect(game.grid).toEqual([['O', null, null],[null, null, null],[null, null, null]]);
     })
 
-    it('Adds a nought to the grid bottom left', () => {
+    it('Adds a cross to the grid bottom left', () => {
       const game = new Game();
-      game.addSymbol([2,0], 'O');
+      game.addSymbol([2,0], 'X');
 
-      expect(game.grid).toEqual([[null, null, null],[null, null, null],['O', null, null]]);
+      expect(game.grid).toEqual([[null, null, null],[null, null, null],['X', null, null]]);
+    })
+  })
+
+  describe('Turn two', () => {
+    it('Adds X to a game with a O in it', () => {
+      const game = new Game();
+      game.addSymbol([0,0], 'O');
+      game.addSymbol([2,0], 'X');
+
+      expect(game.grid).toEqual([['O', null, null],[null, null, null],['X', null, null]]);
+    })
+
+    it('Doesn`t allow another turn in same grid position', () => {
+      const game = new Game();
+
+      game.addSymbol([0,0], 'O');
+
+      expect(() => game.addSymbol([0,0], 'X')).toThrow(Error)
     })
   })
 })
