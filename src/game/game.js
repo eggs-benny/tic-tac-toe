249d2ceb19d69fb24c5a8bb8925a76c972ca98d3
eggs-benny <benedict.smith87@gmail.com>
2023-02-04 14:46:47 +0000

    refactored sub arrays & passed draw test

--- src/game/game.js
+++ src/game/game.js
@@ -1,33 +1,36 @@
 class Game {
   constructor() {
-    this.grid = [[null, null, null],[null, null, null],[null, null, null]]
-    this.symbol = 'X'
+    this.grid = [
+      null, null, null,
+      null, null, null,
+      null, null, null,
+    ];
+    this.symbol = "X";
   }
 
   addSymbol(coords) {
-    if (this.grid[coords[0]][coords[1]] != null) {throw new Error("Error, can't take turn in same position"); 
-  } else {
-    this.grid[coords[0]][coords[1]] = this.symbol
-    return this.grid}
+    if (this.grid[coords] != null) {
+      throw new Error("Error, can't take turn in same position");
+    } else {
+      this.grid[coords] = this.symbol;
+      return this.grid;
+    }
   }
 
   changeSymbol(currentSymbol) {
-    if (currentSymbol === 'O') {
-      this.symbol = 'X'
-    } else {
-      this.symbol = 'O'
-    }
-  } 
+    currentSymbol === "O" ? (this.symbol = "X") : (this.symbol = "O")
+  }
 
   takeTurn(coords) {
-    this.changeSymbol(this.symbol)
-    this.addSymbol(coords)
+    this.changeSymbol(this.symbol);
+    this.addSymbol(coords);
   }
 
   endGame() {
     if (this.grid.indexOf(null) === -1) {
-    return 'Draw!' }
+      return "Draw!";
+    } else { return "BLAH"}
   }
-} 
+}
 
 module.exports = Game;
